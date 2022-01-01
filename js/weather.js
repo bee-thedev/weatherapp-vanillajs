3f8657942689ac4731a496062f8e30b6585980c3
bee-thedev <beenyshsaeed@gmail.com>
2022-01-01 23:20:45 +0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -59,5 +59,32 @@ function fetchData(){
     });
 }
 
-// Functyion for the detailed weather section
+// Function for the detailed weather section
+
+function weatherInformation(info){
+    if(info.cod == "404"){
+        inputText.classList.replace("pending", "error");
+        inputText.innerText = `${inputField.value} is not a valid city`;
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feel_like, humidity} = info.main;
+
+
+        if(id == 800){
+            weatherIcon.src = "images/clear.svg";
+        }else if(id >= 200 && id <= 232){
+            weatherIcon.src = "images/stormy.svg";  
+        }else if(id >= 600 && id <= 622){
+            weatherIcon.src = "images/snowy.svg";
+        }else if(id >= 701 && id <= 781){
+            weatherIcon.src = "images/hazey.svg";
+        }else if(id >= 801 && id <= 804){
+            weatherIcon.src = "images/cloudy.svg";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            weatherIcon.src = "images/rain.svg";
+        }
+    }
+}
 
